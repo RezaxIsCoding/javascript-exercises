23cea0b8e42e8620266678f78cbe047acf63ad4e
Rezax <abdulbaarihindustani@gmail.com>
2024-04-19 21:38:18 +0530

    Done till sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if(typeof a === 'number' && typeof b === 'number'){
+        if(a >= 0 && b >= 0 ){
+            if (a <= b){
+                for(let i = a ; i <= b ; i++){
+                    sum = sum + i;
+                 }
+            }
+            else{
+                for(let i = b ; i <= a ; i++){
+                    sum = sum + i;
+                 }
+            }
+          return sum;
+        }
+        else{
+            return 'ERROR';
+        }
+    }
+    else {
+        return 'ERROR';
+    }
+    
+    
+}
+sumAll(1, 4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(10, "90");
+sumAll(10, [90, 1]);
 // Do not edit below this line
 module.exports = sumAll;
