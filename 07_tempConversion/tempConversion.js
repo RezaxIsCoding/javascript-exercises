262eba1655714e9a86e3656b098cd26187080ae8
Rezax <abdulbaarihindustani@gmail.com>
2024-04-19 21:54:28 +0530

    Done For Array Chap

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahren) {
+  let celsius = Math.round(((tempFahren - 32) * 5/9) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let fahren = Math.round(((tempCelsius * 9/5) + 32) * 10) / 10;
+  return fahren;
 };
-
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
