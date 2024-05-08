2a0ef04211d2baac03488008979904549cd3e15d
Rezax <abdulbaarihindustani@gmail.com>
2024-05-08 19:23:57 +0530

    Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    if (pos < 0){
+        return "OOPS";
+    }
+let n1 = 0 , n2 = 1;
+n = parseInt(pos);
+if (n == 0){
+    return 0;
+}
+else if(n == 1){
+    return 1;
+}
+else{
+    let nextTerm = 0;
+    for(let i = 2 ; i <= n ; i++){
+      nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return nextTerm;
+}
+
 
 };
 
