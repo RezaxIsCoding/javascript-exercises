4cf67532f2c401dd6885e5f08ccf39f2e10d8f48
Rezax <abdulbaarihindustani@gmail.com>
2024-05-11 19:06:38 +0530

    Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    if (people.length === 0) {
+        return null; 
+    }
 
+    let oldestPerson = people[0]; 
+
+    for (let i = 1; i < people.length; i++) {
+        if (getAge(people[i]) > getAge(oldestPerson)) {
+            oldestPerson = people[i]; 
+        }
+    }
+
+    return oldestPerson;
 };
+
+function getAge(person) {
+    const currentYear = new Date().getFullYear();
+    if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return currentYear - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
